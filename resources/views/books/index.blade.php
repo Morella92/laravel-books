0d99513305606ccf38399244d2308f1f3f7efde5
Morella92 <morena.piem@outlook.com>
2023-04-21 16:28:09 +0200

    FEAT index + show

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,5 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
 
+        <div class="btn btn-primary">
+            <a class="btn btn-primary" href="{{route('books.create')}}">Nuovo libro</a>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Autore</th>
+                    <th scope="col">Genere</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Numero Copie</th>
+                    <th scope="col">Editore</th>
+                    <th scope="col">Modifica</th>
+                    <th scope="col">Elimina</th>
+                    <th scope="col">Visualizza</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($books as $book)
+                    <tr>
+                        <th scope="row">
+                            {{$book->id}}
+                        </th>
+                        <td>
+                            {{$book->author}}
+                        </td>
+                        <td>
+                            {{$book->genre}}
+                        </td>
+                        <td>
+                            {{$book->title}}
+                        </td>
+                        <td>
+                            {{$book->copies_number}}
+                        </td>
+                        <td>
+                            {{$book->editor}}
+                        </td>
+                        <td>
+                            <a href="{{route('books.edit', $book)}}" class="btn btn-warning">
+                                Modifica
+                            </a>
+                        </td>
+                        <td>
+                            <form action="{{route('books.destroy', $book)}}" method="POST">
+                                @csrf 
+                                @method('DELETE')
+
+                                <input type="submit" class="btn btn-danger" value="Delete">
+                            </form>
+                        </td>
+                        <td>
+                            <a href="{{route('books.show', $book->id)}}" class="btn btn-success">
+                                Vedi dettagli
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <ul>
+            <li>
+                <h1>{{$book->title}}</h1>
+            </li>
+            <li>
+                <p>{{$book->author}}</p>
+            </li>
+            <li>
+                <p>{{$book->genre}}</p>
+            </li>
+            <li>
+                <p>{{$book->copies_number}}</p>
+            </li>
+            <li>
+                <p>{{$book->editor}}</p>
+            </li>
+        </ul>
+    </div>
 @endsection
